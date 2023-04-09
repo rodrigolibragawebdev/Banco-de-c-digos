a984765bb00d33dc6011a64d1e04dc1990217201
Rodrigo <rodrigo@DESKTOP-0LQCHBF>
2023-04-09 17:51:18 -0300

    Primeira versão banco de códigos

---
+++ PHP/Autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+spl_autoload_register(function ($class) {
+
+  $prefix = "Source\\";
+
+  $baseDir = __DIR__ . "/";
+
+  $len = strlen($prefix);
+
+  if (strncmp($prefix, $class, $len) !== 0) {
+    return;
+  }
+
+  $relativeClass = substr($class, $len);
+
+  $file = str_replace("\\", "/", $baseDir) . str_replace("\\", "/", $relativeClass) . ".php";
+
+  if (file_exists($file)) {
+    require $file;
+  }
+});
